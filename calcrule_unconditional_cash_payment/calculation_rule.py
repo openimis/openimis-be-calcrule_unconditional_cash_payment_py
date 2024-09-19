06f4675d2226a4598493b1d0daa1f98ad1c2b960
delcroip <patrick.delcroix@swisstph.ch>
2024-09-19 14:02:13 +0200

    fix uuid check

--- calcrule_unconditional_cash_payment/calculation_rule.py
+++ calcrule_unconditional_cash_payment/calculation_rule.py
@@ -36,9 +36,9 @@ class UnconditionalCashPaymentCalculationRule(AbsStrategy):
         class_name = instance.__class__.__name__
         match = False
         if class_name == "ABCMeta":
-            match = UUID(cls.uuid) == UUID(instance.uuid)
+            match = UUID(str(cls.uuid)) == UUID(str(instance.uuid))
         if class_name == "PaymentPlan":
-            match = UUID(cls.uuid) == UUID(instance.calculation)
+            match = UUID(str(cls.uuid)) == UUID(str(instance.calculation))
         elif class_name == "Policy":
             match = cls.check_calculation(instance.product)
         elif class_name == "Product":
